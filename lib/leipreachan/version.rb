f66286d1e6e1f16ba87499599168c0a0d9931960
Dmitriy Lyalyuev <dmitriy@lyalyuev.info>
2015-04-28 12:08:48 +0300

    Version up

--- lib/leipreachan/version.rb
+++ lib/leipreachan/version.rb
@@ -1,5 +1,5 @@
 module Leipreachan
-  VERSION = "1.0.1"
+  VERSION = "1.0.2"
   RAILS_VERSION = "3.1.12"
   RUBY_VERSION = IO.read("#{File.dirname(__FILE__)}/../../.ruby-version").strip.split("-").first
 end
