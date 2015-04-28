eced4f20cb2b7f4d6fb3e6670cfa473a92b1f328
Evgeniy Maslov <eum@anahoret.com>
2015-04-28 15:44:00 +0300

    use 'user' instead 'username' add localhost

--- lib/leipreachan/postgresql.rb
+++ lib/leipreachan/postgresql.rb
@@ -1,29 +1,30 @@
 module Leipreachan
   class Backuper < DBBackup
     def dbbackup!
-      username = db_config['username'].present? ? "-U #{db_config['username']}" : ""
+      user = db_config['user'].present? ? "-U #{db_config['user']}" : ""
       password = db_config['password'].present? ? "PGPASSWORD='#{db_config['password']}' " : ""
-      system("#{password}pg_dump #{username} #{db_config['database']} | gzip > #{backup_file}.gz")
+
+      system("#{password}pg_dump -h localhost #{user} #{db_config['database']} | gzip > #{backup_file}.gz")
     end
 
     def dbrestore! file
-      username = db_config['username'].present? ? "-U #{db_config['username']}" : ""
+      user = db_config['user'].present? ? "-U #{db_config['user']}" : ""
       password = db_config['password'].present? ? "PGPASSWORD='#{db_config['password']}' " : ""
 
       puts "Will be restored -> #{file}"
       puts ""
       drop_pg!
-      system("zcat < #{backup_base_on(backup_folder)}/#{file} | #{password}psql #{username} #{db_config['database']}")
+      system("zcat < #{backup_base_on(backup_folder)}/#{file} | #{password}psql -h localhost #{user} #{db_config['database']}")
     end
 
     private
 
     def drop_pg!
-      username = db_config['username'].present? ? "-U #{db_config['username']}" : ""
+      user = db_config['user'].present? ? "-U #{db_config['user']}" : ""
       password = db_config['password'].present? ? "PGPASSWORD='#{db_config['password']}' " : ""
       drop_table_query = "drop schema public cascade; create schema public;"
 
-      system("echo \"#{drop_table_query}\" | #{password}psql #{username} #{db_config['database']}")
+      system("echo \"#{drop_table_query}\" | #{password}psql -h localhost #{user} #{db_config['database']}")
     end
   end
 end
