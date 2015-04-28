0da6fc6cc6c89565e1f9bbff344b06082af43f1c
Evgeniy Maslov <eum@anahoret.com>
2015-04-28 15:49:05 +0300

    extract credentials

--- lib/leipreachan/postgresql.rb
+++ lib/leipreachan/postgresql.rb
@@ -1,30 +1,33 @@
 module Leipreachan
   class Backuper < DBBackup
-    def dbbackup!
-      user = db_config['user'].present? ? "-U #{db_config['user']}" : ""
-      password = db_config['password'].present? ? "PGPASSWORD='#{db_config['password']}' " : ""
+    def user
+      @user ||= db_config['user'].present? ? "-U #{db_config['user']}" : ""
+    end
 
-      system("#{password}pg_dump -h localhost #{user} #{db_config['database']} | gzip > #{backup_file}.gz")
+    def password
+      @password ||= db_config['password'].present? ? "PGPASSWORD='#{db_config['password']}' " : ""
+    end
+
+    def host
+      @host ||= db_config['host'].present? ? db_config['host'] : "localhost"
+    end
+
+    def dbbackup!
+      system("#{password}pg_dump -h #{host} #{db_config['database']} | gzip > #{backup_file}.gz")
     end
 
     def dbrestore! file
-      user = db_config['user'].present? ? "-U #{db_config['user']}" : ""
-      password = db_config['password'].present? ? "PGPASSWORD='#{db_config['password']}' " : ""
-
       puts "Will be restored -> #{file}"
       puts ""
       drop_pg!
-      system("zcat < #{backup_base_on(backup_folder)}/#{file} | #{password}psql -h localhost #{user} #{db_config['database']}")
+      system("zcat < #{backup_base_on(backup_folder)}/#{file} | #{password}psql -h #{host} #{user} #{db_config['database']}")
     end
 
     private
 
     def drop_pg!
-      user = db_config['user'].present? ? "-U #{db_config['user']}" : ""
-      password = db_config['password'].present? ? "PGPASSWORD='#{db_config['password']}' " : ""
       drop_table_query = "drop schema public cascade; create schema public;"
-
-      system("echo \"#{drop_table_query}\" | #{password}psql -h localhost #{user} #{db_config['database']}")
+      system("echo \"#{drop_table_query}\" | #{password}psql -h #{host} #{user} #{db_config['database']}")
     end
   end
 end
