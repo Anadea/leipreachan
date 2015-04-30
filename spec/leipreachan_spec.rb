3a227f4ef7079989162c05ebc048ec7db520a0f0
Dmitriy Lyalyuev <dmitriy@lyalyuev.info>
2015-04-30 13:52:33 +0300

    Tests for system checks

--- spec/leipreachan_spec.rb
+++ spec/leipreachan_spec.rb
@@ -50,6 +50,34 @@ describe Leipreachan do
     FileUtils.rm_rf(Rails.root)
   end
 
+  context "MySQL: Backup and restore with password" do
+    before do
+      ActiveRecord::Base.stub(:configurations).and_return({'test' => {'adapter' => 'mysql2', 'username' => 'login', 'password' => 'password', 'database' => 'dbname'}})
+      instance.stub(:backup_base_on).and_return('.')
+      instance.stub(:backup_file).and_return('201504040000.sql')
+    end
+
+    let!(:instance) { Leipreachan.get_backuper_for Rails.env }
+    it { expect(instance.respond_to?(:system_check_list)).to be_truthy }
+
+    it 'Backup' do
+      instance.stub(:system) { |arg| arg }
+      expect(instance.send(:dbbackup!)).to eq "mysqldump -h localhost -ulogin -ppassword -i -c -q --single-transaction dbname | gzip > 201504040000.sql.gz"
+    end
+
+    it 'Drop old content' do
+      instance.stub(:system) { |arg| arg }
+      instance.stub(:get_file_for_restore).and_return('20150404000000.sql.gz')
+      expect(instance.send(:drop_tables!)).to eq "mysql --silent --skip-column-names -e \"SHOW TABLES\" -h localhost -ulogin -ppassword dbname | xargs -L1 -I% echo 'DROP TABLE `%`;' | mysql -v -h localhost -ulogin -ppassword dbname"
+    end
+
+    it 'Restore' do
+      instance.stub(:system) { |arg| arg }
+      instance.stub(:get_file_for_restore).and_return('20150404000000.sql.gz')
+      expect(instance.send(:restore!)).to eq "zcat < ./#{instance.send(:get_file_for_restore)} | mysql -h localhost -ulogin -ppassword dbname"
+    end
+  end
+
   context 'MySQL: Backup and restore database without password' do
     before do
       ActiveRecord::Base.stub(:configurations).and_return({'test' => {'adapter' => 'mysql2', 'username' => 'login', 'password' => '', 'database' => 'dbname'}})
@@ -77,33 +105,6 @@ describe Leipreachan do
     end
   end
 
-  context "MySQL: Backup and restore with password" do
-    before do
-      ActiveRecord::Base.stub(:configurations).and_return({'test' => {'adapter' => 'mysql2', 'username' => 'login', 'password' => 'password', 'database' => 'dbname'}})
-      instance.stub(:backup_base_on).and_return('.')
-      instance.stub(:backup_file).and_return('201504040000.sql')
-    end
-
-    let!(:instance) { Leipreachan.get_backuper_for Rails.env }
-
-    it 'Backup' do
-      instance.stub(:system) { |arg| arg }
-      expect(instance.send(:dbbackup!)).to eq "mysqldump -h localhost -ulogin -ppassword -i -c -q --single-transaction dbname | gzip > 201504040000.sql.gz"
-    end
-
-    it 'Drop old content' do
-      instance.stub(:system) { |arg| arg }
-      instance.stub(:get_file_for_restore).and_return('20150404000000.sql.gz')
-      expect(instance.send(:drop_tables!)).to eq "mysql --silent --skip-column-names -e \"SHOW TABLES\" -h localhost -ulogin -ppassword dbname | xargs -L1 -I% echo 'DROP TABLE `%`;' | mysql -v -h localhost -ulogin -ppassword dbname"
-    end
-
-    it 'Restore' do
-      instance.stub(:system) { |arg| arg }
-      instance.stub(:get_file_for_restore).and_return('20150404000000.sql.gz')
-      expect(instance.send(:restore!)).to eq "zcat < ./#{instance.send(:get_file_for_restore)} | mysql -h localhost -ulogin -ppassword dbname"
-    end
-  end
-
   context 'Postgres: Backup and restore with password' do
     before do
       ActiveRecord::Base.stub(:configurations).and_return({'test' => {'adapter' => 'postgresql', 'user' => 'login', 'password' => 'password', 'database' => 'dbname'}})
@@ -112,6 +113,7 @@ describe Leipreachan do
     end
 
     let!(:instance) { Leipreachan.get_backuper_for Rails.env }
+    it { expect(instance.respond_to?(:system_check_list)).to be_truthy }
 
     it 'Backup' do
       instance.stub(:system) { |arg| arg }
@@ -169,6 +171,19 @@ describe Leipreachan do
 
     it 'backup_base_on return correct array' do
       expect(instance.send(:backup_folder_items)).to eq(['20150404000000.sql.gz', '20150403000000.sql.gz'])
+    end
+  end
+
+  context 'Exceptions checks' do
+    before do
+      ActiveRecord::Base.stub(:configurations).and_return({'test' => {'adapter' => 'postgresql', 'username' => 'login', 'password' => 'password', 'database' => 'dbname'}})
+    end
+
+    let!(:instance) { Leipreachan.get_backuper_for Rails.env }
+
+    it 'Check not found command' do
+      instance.stub(:system_check_list).and_return(['11243111'])
+      expect { instance.check_system_requirements! }.to raise_error(RuntimeError, '11243111 is required for Leipreachan backups')
     end
   end
 end
